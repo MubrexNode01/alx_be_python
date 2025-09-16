e4303acc14d51b90fbe3df5d525fdb03c96a7329
Mubarak <mmubarakadebayo@gmail.com>
2025-09-16 03:50:40 +0100

    Fixed the error handling bug with future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,17 @@
-#calculates the age of a user-input age
-age = int (input ("how old are you: "))
-age_in_2050 =  age + (2050 - 2023)
-print ("In 2050, you will be", age_in_2050, "years old")
+# Calculates the age of a user in future year eg 2050
+
+future_year = 2050
+current_year = 2023
+
+while True:
+    user_input = input("How old are you: ")
+    
+    if user_input.isdigit():   # check if input is only numbers
+        age = int(user_input)
+        break
+    else:
+        print("Please enter numbers only!")
+
+age_in_2050 = age + (future_year - current_year)
+print("In 2050, you will be", age_in_2050, "years old")
+
