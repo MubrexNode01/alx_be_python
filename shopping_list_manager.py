b08c048f53f68bbad19ef342f988e018f965b590
Mubarak <mmubarakadebayo@gmail.com>
2025-09-28 23:47:56 +0100

    added shopping_list_manager.py

--- shopping_list_manager.py
+++ shopping_list_manager.py
@@ -1,11 +1,11 @@
-#shopping list manager
+#A shopping list manager for your next shopping outting
 def display_menu():
     print("\nShopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-    
+
 def main():
     shopping_list = []
 
