ba8d494210f84b9ef4c615e2a05d81a8519c0d79
Mubarak <mmubarakadebayo@gmail.com>
2025-10-18 06:45:16 +0100

    fixed the bug with arithmetic_operations.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -18,20 +18,4 @@ def operation (a, b, operations):
     
 def perform_operation(num1, num2, operations):
     return operation(num1, num2, operations)  
-    
-
-
-#3main.py      
-from arithmetic_operations import perform_operation
-
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
-if __name__ == "__main__":
-    main()
+ 
